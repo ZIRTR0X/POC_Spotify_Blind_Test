843d10573237ae8d4dcb0336797e68d80dc10c89
Julien THEME <julien.theme@etu.uca.fr>
2024-02-13 02:07:23 +0100

    💄 update ui

--- poc_spotitest/src/pages/index.tsx
+++ poc_spotitest/src/pages/index.tsx
@@ -1,67 +1,108 @@
-import type { NextPage } from 'next';
-import { signIn, signOut, useSession } from 'next-auth/react';
-import Head from 'next/head';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import ReactPlayer from 'react-player';
-import { ITrack } from '~/models/itrack';
+import type { NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Head from "next/head";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ReactPlayer from "react-player";
+import { ITrack } from "~/models/itrack";
+import {
+  Avatar,
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Input,
+  Link,
+  Navbar,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+  NextUIProvider,
+  User,
+} from "@nextui-org/react";
+import Image from "next/image";
 
-const SPOTIFY_LIBRARY_ENDPOINT = 'https://api.spotify.com/v1/me/tracks';
+const SPOTIFY_LIBRARY_ENDPOINT = "https://api.spotify.com/v1/me/tracks";
 
 const Home: NextPage = () => {
   const session = useSession();
-  const [playingTrack, setPlayingTrack] = useState(null) as [ITrack | null, Function];
-  const [userAnswer, setUserAnswer] = useState('');
+  const [playingTrack, setPlayingTrack] = useState(null) as [
+    ITrack | null,
+    Function,
+  ];
+  const [userAnswer, setUserAnswer] = useState("");
   const [score, setScore] = useState(0);
   const [artistsFoundNumber, setArtistsFoundNumber] = useState(0);
   const [artistsFound, setArtistsFound] = useState([] as string[]);
   const [totalArtistsNumber, setTotalArtistsNumber] = useState(0);
-  const [musicTitleFound, setMusicTitleFound] = useState('');
+  const [musicTitleFound, setMusicTitleFound] = useState("");
+  const spotifyLogo = (
+    <svg
+      width="24"
+      height="24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+    >
+      <path d="M19.098 10.638c-3.868-2.297-10.248-2.508-13.941-1.387-.593.18-1.22-.155-1.399-.748-.18-.593.154-1.22.748-1.4 4.239-1.287 11.285-1.038 15.738 1.605.533.317.708 1.005.392 1.538-.316.533-1.005.709-1.538.392zm-.126 3.403c-.272.44-.847.578-1.287.308-3.225-1.982-8.142-2.557-11.958-1.399-.494.15-1.017-.129-1.167-.623-.149-.495.13-1.016.624-1.167 4.358-1.322 9.776-.682 13.48 1.595.44.27.578.847.308 1.286zm-1.469 3.267c-.215.354-.676.465-1.028.249-2.818-1.722-6.365-2.111-10.542-1.157-.402.092-.803-.16-.895-.562-.092-.403.159-.804.562-.896 4.571-1.045 8.492-.595 11.655 1.338.353.215.464.676.248 1.028zm-5.503-17.308c-6.627 0-12 5.373-12 12 0 6.628 5.373 12 12 12 6.628 0 12-5.372 12-12 0-6.627-5.372-12-12-12z" />
+    </svg>
+  );
 
   useEffect(() => {
-    if (session.status === 'authenticated') {
+    if (session.status === "authenticated") {
       getRandomSongFromLibrary(session, setPlayingTrack);
     }
   }, [session]);
 
-  async function getRandomSongFromLibrary(session: any, setPlayingTrack: Function) {
+  async function getRandomSongFromLibrary(
+    session: any,
+    setPlayingTrack: Function,
+  ) {
     try {
       const accessToken = session.data.user.accessToken;
-  
+
       const response = await axios.get(SPOTIFY_LIBRARY_ENDPOINT, {
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
       });
-  
+
       const tracks = response.data.items.map((item: any) => item.track);
-  
+
       if (tracks.length === 0) {
-        console.log('User has no tracks in their library.');
+        console.log("User has no tracks in their library.");
         return;
       }
-  
+
       const randomTrack = tracks[Math.floor(Math.random() * tracks.length)];
-  
-      console.log('Random Track:', {
+
+      console.log("Random Track:", {
         name: randomTrack.name,
-        artist: randomTrack.artists.map((artist: any) => artist.name).join(', '),
+        artist: randomTrack.artists
+          .map((artist: any) => artist.name)
+          .join(", "),
         uri: randomTrack.uri,
       });
-  
+
       setPlayingTrack(randomTrack);
-  
+
       initMusicStats(randomTrack);
-  
     } catch (error: any) {
-      console.error('Error fetching user library:', error.response?.data || error.message);
+      console.error(
+        "Error fetching user library:",
+        error.response?.data || error.message,
+      );
     }
   }
 
-  function initMusicStats(randomTrack: ITrack) { //TODO: créer une interface pour randomTrack
+  function initMusicStats(randomTrack: ITrack) {
+    //TODO: créer une interface pour randomTrack
     setArtistsFound([]);
     setArtistsFoundNumber(0);
-    setMusicTitleFound('');
+    setMusicTitleFound("");
     setTotalArtistsNumber(randomTrack.artists.length || 0);
   }
 
@@ -72,36 +113,42 @@ const Home: NextPage = () => {
   const handleSubmitAnswer = () => {
     handleAnswer(userAnswer);
 
-    if(checkEndGame())
-      getRandomSongFromLibrary(session, setPlayingTrack);
+    if (checkEndGame()) getRandomSongFromLibrary(session, setPlayingTrack);
   };
 
   /** Return `true` if all artists are found and the music title is found. */
   function checkEndGame(): boolean {
-    return (artistsFoundNumber === totalArtistsNumber && musicTitleFound !== '');
+    return artistsFoundNumber === totalArtistsNumber && musicTitleFound !== "";
   }
 
   /** Handle the user's answer and update the score.
    * @param answer The user's answer.
    */
   function handleAnswer(answer: string) {
-    if(!playingTrack) return;
+    if (!playingTrack) return;
 
-    console.log('playingTrack:', playingTrack);
+    console.log("playingTrack:", playingTrack);
 
-    if(checkMusicTitle(answer)) {
-      console.log('Title found:', playingTrack.name);
-      
+    if (checkMusicTitle(answer)) {
+      console.log("Title found:", playingTrack.name);
+
       setScore(score + 100);
-    }
-    else {
+    } else {
       const artistsFound = getArtistsFoundNumber(answer);
       const artistsNumber = playingTrack.artists.length;
       const artistsPercentage = (artistsFound / artistsNumber) * 100;
-      
-      if(artistsFound > 0) {
-        console.log('Artists found:', artistsFound, '/', artistsNumber, '(', artistsPercentage, '%)');
-        
+
+      if (artistsFound > 0) {
+        console.log(
+          "Artists found:",
+          artistsFound,
+          "/",
+          artistsNumber,
+          "(",
+          artistsPercentage,
+          "%)",
+        );
+
         setScore(score + artistsPercentage);
       }
     }
@@ -112,11 +159,16 @@ const Home: NextPage = () => {
    * @param userAnswer The user's answer.
    * @returns The match percentage.
    */
-  function calculateTitleMatchPercentage(songTitle: string, userAnswer: string): number {
+  function calculateTitleMatchPercentage(
+    songTitle: string,
+    userAnswer: string,
+  ): number {
     const normalizedSongTitle: string = songTitle.toLowerCase();
     const normalizedUserAnswer: string = userAnswer.toLowerCase();
-    const matchPercentage: number = parseFloat((similarity(normalizedSongTitle, normalizedUserAnswer) * 100).toFixed(2));
-  
+    const matchPercentage: number = parseFloat(
+      (similarity(normalizedSongTitle, normalizedUserAnswer) * 100).toFixed(2),
+    );
+
     return matchPercentage;
   }
 
@@ -130,10 +182,12 @@ const Home: NextPage = () => {
     const shorter: string = stringA.length > stringB.length ? stringB : stringA;
     const longerLength: number = longer.length;
 
-    if (longerLength === 0)
-      return 1.0;
+    if (longerLength === 0) return 1.0;
     else
-      return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength.toString());
+      return (
+        (longerLength - editDistance(longer, shorter)) /
+        parseFloat(longerLength.toString())
+      );
   }
 
   /** Calculate the edit distance between two strings.
@@ -151,9 +205,7 @@ const Home: NextPage = () => {
       let lastValue: number = i;
 
       for (let j = 0; j <= lowerCaseStringB.length; j++) {
-        if (i === 0)
-          costs[j] = j;
-
+        if (i === 0) costs[j] = j;
         else if (j > 0) {
           let newValue: number = costs[j - 1]!;
 
@@ -178,8 +230,11 @@ const Home: NextPage = () => {
    * @returns `true` if the user's answer matches the music title, `false` otherwise.
    */
   function checkMusicTitle(answer: string): boolean {
-    if (playingTrack && musicTitleFound === '') {
-      const matchPercentage: number = calculateTitleMatchPercentage(playingTrack.name, answer);
+    if (playingTrack && musicTitleFound === "") {
+      const matchPercentage: number = calculateTitleMatchPercentage(
+        playingTrack.name,
+        answer,
+      );
 
       if (matchPercentage >= 80) {
         setMusicTitleFound(playingTrack.name);
@@ -195,128 +250,155 @@ const Home: NextPage = () => {
    */
   function getArtistsFoundNumber(answer: string): number {
     if (playingTrack) {
-      const artists: string[] = playingTrack.artists.map((artist: any) => artist.name);
+      const artists: string[] = playingTrack.artists.map(
+        (artist: any) => artist.name,
+      );
       const normalizedAnswer: string = answer.toLowerCase();
-      const matchingArtists: string[] = artists.filter((artist) => normalizedAnswer.includes(artist.toLowerCase()));
-      const newArtistsFound: string[] = matchingArtists.filter((artist) => !artistsFound.includes(artist));
+      const matchingArtists: string[] = artists.filter((artist) =>
+        normalizedAnswer.includes(artist.toLowerCase()),
+      );
+      const newArtistsFound: string[] = matchingArtists.filter(
+        (artist) => !artistsFound.includes(artist),
+      );
 
-      console.log('New artists found:', newArtistsFound);
+      console.log("New artists found:", newArtistsFound);
 
-      if(newArtistsFound.length > 0) {
+      if (newArtistsFound.length > 0) {
         setArtistsFound((prevArtists) => [...prevArtists, ...newArtistsFound]);
-        setArtistsFoundNumber((prevNumber) => prevNumber + matchingArtists.length);
+        setArtistsFoundNumber(
+          (prevNumber) => prevNumber + matchingArtists.length,
+        );
       }
 
       return newArtistsFound.length;
-    } 
-    else
-      return 0;
+    } else return 0;
   }
-  
 
   return (
-    <div className="flex flex-col h-screen">
+    <NextUIProvider className="min-h-screen bg-zinc-900 font-mono text-white">
       <Head>
-        <title>SpotiGame</title>
+        <title>Blind Test Spotify</title>
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
- <main className="flex-grow flex flex-col">
-        <div className="bg-gray-800 p-4 flex justify-between items-center">
+      <Navbar className="bg-purple-500" isBlurred={true}>
+        <NavbarContent justify="start">
+          <NavbarItem>
+            <p className="text-lg text-white">Blind Test Spotify</p>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent justify="end">
+          {session?.data && (
+            <NavbarItem className="flex items-center gap-3">
+              <Dropdown>
+                <DropdownTrigger>
+                  <Button className="bg-purple-200 py-6">
+                    <User
+                      avatarProps={{ src: session?.data?.user.image ?? "" }}
+                      name={session?.data?.user.name ?? ""}
+                    />
+                  </Button>
+                </DropdownTrigger>
+                <DropdownMenu>
+                  <DropdownItem
+                    key="signout"
+                    color="danger"
+                    onClick={() => signOut()}
+                  >
+                    <p>Déconnexion</p>
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </NavbarItem>
+          )}
+        </NavbarContent>
+      </Navbar>
 
-          <h1 className="text-white text-lg">
-            BONJOUR !{' '}
-            {session.status === 'authenticated'
-              ? session.data.user?.name || ''
-              : 'Veuillez vous connecter'}
-          </h1>
-          <div>
-            {session.status === 'authenticated' ? (
-              <>
-                <button
-                  type="button"
-                  onClick={() => signOut()}
-                  className="bg-gray-200 text-black px-2 py-1 rounded"
-                >
-                  Déconnexion {session.data.user?.email}
-                </button>
-
-                
-              </>
-            ) : (
-              <button
-                type="button"
-                onClick={() => signIn('spotify')}
-                disabled={session.status === 'loading'}
-                className="bg-gray-200 text-black px-2 py-1 rounded"
+      <main className="flex h-max flex-grow flex-col">
+        <div className="flex flex-col items-center justify-center space-y-4">
+          {!playingTrack && (
+            <div className="mb-40 flex flex-col items-center justify-center space-y-4">
+              <p className="my-20 text-center text-2xl font-bold">
+                Veuillez vous connecter avec Spotify pour commencer à jouer.
+              </p>
+              <Button
+                color="success"
+                onClick={() => signIn("spotify")}
+                disabled={session.status === "loading"}
+                startContent={spotifyLogo}
               >
-                Connexion avec Spotify
-              </button>
-            )}
-          </div>
+                <p className="font-semibold text-black">Connexion</p>
+              </Button>
+            </div>
+          )}
         </div>
 
-        <div className="bg-gray-300 flex flex-col items-center justify-center space-y-4 flex-grow">
+        <div className="flex flex-grow flex-col items-center justify-center space-y-4">
           {playingTrack && (
-            <div className="mx-auto">
+            <div className="mx-auto mt-20">
               <img
-                src={playingTrack.album.images[0].url} // Assuming the first image of the album is used
+                src={playingTrack?.album.images[0]?.url} // Assuming the first image of the album is used
                 alt={`Album cover for ${playingTrack.name}`}
-                className="m-auto w-80 h-80"
+                className="m-auto h-80 w-80 rounded-lg"
               />
               <ReactPlayer
                 url={playingTrack.preview_url}
                 playing={true}
                 controls={true}
                 volume={1}
-                className="h-20"
+                width={320}
+                height={50}
+                className="mb-5 mt-10"
               />
             </div>
           )}
         </div>
-        <div className="bg-gray-200">
+        <div>
           {playingTrack && (
- 
-          <div className="flex flex-col items-center space-y-2 p-5">
+            <div className="flex flex-col items-center space-y-2 p-5 text-white">
+              <div className="mb-5 flex flex-col items-center">
                 <p>Score : {score}</p>
-                <p>Artists : {artistsFoundNumber}/{totalArtistsNumber}</p>
-                <p>Titre : {musicTitleFound}</p>
-                <form>
-                  <label className="text-lg mx-2">
-                    Votre proposition :
-                    <input
-                      type="text"
-                      value={userAnswer}
-                      onChange={handleAnswerChange}
-                      className="border border-gray-300 px-2 py-1 mx-2 rounded"
-                    />
-                  </label>
-                  <button
+                <p>
+                  Artists : {artistsFoundNumber}/{totalArtistsNumber}
+                </p>
+                <p>Titre : {musicTitleFound !== "" ? musicTitleFound : "?"}</p>
+              </div>
+              <form>
+                <Input
+                  type="text"
+                  variant="bordered"
+                  defaultValue="Réponse"
+                  onChange={handleAnswerChange}
+                  value={userAnswer}
+                  className="w-80"
+                />
+                <div className="flex flex-row justify-center">
+                  <Button
                     type="button"
                     onClick={handleSubmitAnswer}
-                    className="bg-gray-500 text-white px-4 py-2 mx-2 rounded"
+                    className="mx-2 my-4 rounded bg-purple-500 px-4 py-2 text-lg text-white"
                   >
                     Soumettre
-                  </button>
+                  </Button>
 
-                  <button
+                  <Button
                     type="button"
-                    onClick={() => getRandomSongFromLibrary(session, setPlayingTrack)}
-                    className="bg-gray-500 text-white px-4 py-2 mx-2 rounded"
-                  > 
+                    onClick={() =>
+                      getRandomSongFromLibrary(session, setPlayingTrack)
+                    }
+                    className="mx-2 my-4 rounded bg-purple-500 px-4 py-2 text-lg text-white"
+                  >
                     Passer
-                  </button>
-                </form>
-              </div>
-              
+                  </Button>
+                </div>
+              </form>
+            </div>
           )}
         </div>
-
       </main>
-    </div>
+    </NextUIProvider>
   );
-
 };
 
 export default Home;
